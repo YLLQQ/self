0dc29ab8cbdb661853f5833e1c8c541993ea7dbc
YLLQQ <ysh1059@163.com>
2019-01-05 17:26:34 +0800

    多数据源配置

--- web/src/main/java/self/yang/web/WebApplication.java
+++ web/src/main/java/self/yang/web/WebApplication.java
@@ -1,10 +1,10 @@
 package self.yang.web;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 public class WebApplication {
 
     public static void main(String[] args) {
