c996781fcc34019d254c20830e1144c174e6f5b5
ElevenYang <ysh1059@163.com>
2019-01-08 23:09:18 +0800

    hashMap结构

---
+++ demos/src/main/java/collections/HashMapMain.java
@@ -0,0 +1,21 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapMain {
+
+    public static void main(String[] args) {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("1", "1");
+
+//        System.out.println(map.get("1"));
+//
+//        Set<Map.Entry<String, Object>> entries = map.entrySet();
+//
+//        for (Map.Entry<String, Object> entry : entries) {
+//            System.out.println(entry.getKey() + " " + entry.getValue());
+//        }
+    }
+}
