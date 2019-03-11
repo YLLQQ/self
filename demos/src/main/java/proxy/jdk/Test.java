70e3b5b1ed2011d72767753726fdf9162791bc31
YLLQQ <ysh1059@163.com>
2019-03-11 19:12:01 +0800

    解析jdk动态代理流程

--- demos/src/main/java/proxy/jdk/Test.java
+++ demos/src/main/java/proxy/jdk/Test.java
@@ -5,8 +5,23 @@ import java.lang.reflect.Proxy;
 public class Test {
 
     public static void main(String[] args) {
+        /**
+         * 把生成的代理类Class文件保存在本地磁盘上
+         */
+        System.getProperties().put("sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+
         Subject subject = new SubjectImpl();
 
+        /**
+         * newProxyInstance方法执行了以下几种操作。
+         *
+         * 1.生成一个实现了参数interfaces里所有接口且继承了Proxy的代理类的字节码，然后用参数里的classLoader加载这个代理类。
+         *
+         * 2.使用代理类父类的构造函数 Proxy(InvocationHandler h)来创造一个代理类的实例，将我们自定义的InvocationHandler的子类传入。
+         *
+         * 3.返回这个代理类实例。
+         *
+         */
         Subject subjectProxy = (Subject) Proxy.newProxyInstance(
                 subject.getClass().getClassLoader(),
                 subject.getClass().getInterfaces(),
