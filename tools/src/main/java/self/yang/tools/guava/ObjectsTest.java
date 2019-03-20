0147dab2d5feb7914b199d6f174d26ae515bd5c2
yangguoqing <yangguoqing@zjptmc.com>
2019-03-20 11:03:18 +0800

    Objects测试

--- tools/src/main/java/self/yang/tools/guava/ObjectsTest.java
+++ tools/src/main/java/self/yang/tools/guava/ObjectsTest.java
@@ -1,6 +1,8 @@
 package self.yang.tools.guava;
 
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
+import com.google.common.collect.ComparisonChain;
 
 public class ObjectsTest {
 
@@ -20,8 +22,39 @@ public class ObjectsTest {
         //false
         System.out.println(Objects.equal(null, "b"));
 
-//        String toString = Objects.toStringHelper(this).add("x", 1).toString();
-//
-//        System.out.println(toString);
+        //ObjectsTest{x=1}
+        String toString = MoreObjects.toStringHelper(new ObjectsTest()).add("x", 1).toString();
+
+        System.out.println(toString);
+
+        //ObjectsTest{x=1}
+        toString = MoreObjects.toStringHelper("ObjectsTest").add("x", 1).toString();
+
+        System.out.println(toString);
+
+        //{x=1}
+        toString = MoreObjects.toStringHelper("").add("x", 1).toString();
+
+        System.out.println(toString);
+
+        //1
+        int result = ComparisonChain.start().compareFalseFirst(true, false).result();
+
+        System.out.println(result);
+
+        //-1
+        result = ComparisonChain.start().compareFalseFirst(false, true).result();
+
+        System.out.println(result);
+
+        //0
+        result = ComparisonChain.start().compareFalseFirst(false, false).result();
+
+        System.out.println(result);
+
+        //0
+        result = ComparisonChain.start().compareFalseFirst(true, true).result();
+
+        System.out.println(result);
     }
 }
