af1f46ad2d262753cabdd48f4af33eb11252cc18
yangguoqing <yangguoqing@zjptmc.com>
2019-04-18 20:09:59 +0800

    修改拦截器代码

--- web/src/main/java/self/yang/web/interceptor/DefineWebMvcConfig.java
+++ web/src/main/java/self/yang/web/interceptor/DefineWebMvcConfig.java
@@ -1,8 +1,14 @@
 package self.yang.web.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * self.yang.web.interceptor.DefineWebMvcConfig
@@ -10,12 +16,41 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author eleven
  * @date 2019/04/18
  */
+@Slf4j
 @Configuration
 public class DefineWebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new DefineInterceptor())
-                .addPathPatterns("/**");
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(
+                    HttpServletRequest request, HttpServletResponse response, Object handler
+            ) throws Exception {
+                if (log.isDebugEnabled()) {
+                    log.debug("enter pre handle");
+                }
+
+                return true;
+            }
+
+            @Override
+            public void postHandle(
+                    HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView
+            ) throws Exception {
+                if (log.isDebugEnabled()) {
+                    log.debug("enter post handle");
+                }
+            }
+
+            @Override
+            public void afterCompletion(
+                    HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex
+            ) throws Exception {
+                if (log.isDebugEnabled()) {
+                    log.debug("after pre handle");
+                }
+            }
+        }).addPathPatterns("/**");
     }
 }
